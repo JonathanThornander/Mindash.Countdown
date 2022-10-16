7342cb71b25e24c131afc77bd8752e74a32993fb
Jonathan Thornander <43991450+JonathanThornander@users.noreply.github.com>
2022-10-16 19:14:51 +0200

    Add project files.

---
+++ Mindash.Countdown.Web/Data/CountDownContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mindash.Countdown.Web.Data.DTOS;
+
+namespace Mindash.Countdown.Web.Data
+{
+    public class CountDownDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Data/database.db");
+        }
+
+        public DbSet<DTOS.CountdownEvent> CountdownEvents { get; set; }
+    }
+}
